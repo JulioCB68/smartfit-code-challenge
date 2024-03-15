eb9d048272fe519b863038abd94de0ec4b8a1720
Júlio César <juliobento237@gmail.com>
2024-03-15 15:52:12 -0300

    fix: fixing card gym layoult

--- src/components/gym.tsx
+++ src/components/gym.tsx
@@ -21,18 +21,20 @@ export default function Gym({ data }: { data: GymI }) {
   const lockerRoom = data.locker_room
   const cityName = data.city_name
 
-  console.log(data)
   return (
-    <div className="my-8 h-full w-full">
+    <div className="my-8 h-[27rem] w-96 rounded-lg border border-gray-300 p-4">
       <p
         className={`font-medium ${opened === true ? 'text-green-600' : 'text-red-600'}`}
       >
         {opened === true ? 'Aberto' : 'Fechado'}
       </p>
-      <h1>{title}</h1>
+      <h1
+        className="py-2 text-2xl font-bold"
+        dangerouslySetInnerHTML={{ __html: title }}
+      />
       {street && (
         <>
-          <p className="flex">
+          <p className="flex text-light-grey">
             {street} - {region} - {cityName}, {uf}
           </p>
           <p className="pt-2 font-semibold">
@@ -41,36 +43,39 @@ export default function Gym({ data }: { data: GymI }) {
         </>
       )}
 
-      {!street && (
+      {content && (
         <>
-          <p className="" dangerouslySetInnerHTML={{ __html: content }} />
+          <p
+            className="pb-4 text-light-grey"
+            dangerouslySetInnerHTML={{ __html: content }}
+          />
 
-          <ul className="flex">
+          <div className="my-4 flex gap-2">
             <Image
-              width={30}
-              height={30}
+              width={35}
+              height={35}
               src={`/assets/images/${mask}-mask.png`}
               alt="Mask logo"
             />
             <Image
-              width={25}
-              height={25}
+              width={35}
+              height={35}
               src={`/assets/images/${towel}-towel.png`}
               alt="Towel logo"
             />
             <Image
-              width={25}
-              height={25}
+              width={35}
+              height={35}
               src={`/assets/images/${fountain}-fountain.png`}
               alt="Fountain logo"
             />
             <Image
-              width={25}
-              height={25}
+              width={35}
+              height={35}
               src={`/assets/images/${lockerRoom}-lockerroom.png`}
               alt="Locker room logo"
             />
-          </ul>
+          </div>
 
           <div className="flex flex-wrap gap-4">
             {schedules?.map((item, index) => (
